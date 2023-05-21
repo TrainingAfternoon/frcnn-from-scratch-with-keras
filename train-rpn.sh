1de1beb155f22c50d83509b9eeab68620a815361
Sam" "Keyser <keysers@dh-mgmt2.hpc.msoe.edu>
2023-05-20 19:32:39 -0500

    Add in train rpn scripts

---
+++ train-rpn.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --job-name='rpn train'
+#SBATCH --partition=teaching
+#SBATCH --nodes=1
+#SBATCH --gpus=4
+#SBATCH --cpus-per-gpu=16
+#SBATCH --time=1-0:0
+# time format: <days>-<hours>:<minutes>
+
+# set up the environment
+source /usr/local/anaconda3/bin/activate keras-frcnn
+
+# command
+python3 train_rpn.py --network vgg --num_epochs 20 -o simple -p annotations.csv
+
+# deactivate env
+conda deactivate
